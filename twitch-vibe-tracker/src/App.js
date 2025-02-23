a75d56cc935d3ab8738a2c04bac1ae757f2af4dc
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2025-02-22 21:22:14 -0800

    Fixed API URL for GitHub Pages

--- twitch-vibe-tracker/src/App.js
+++ twitch-vibe-tracker/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css"; // Importing the CSS file
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000";
+const API_BASE_URL = "https://crowdsync.onrender.com";
 
 const App = () => {
   const [channel, setChannel] = useState("");
